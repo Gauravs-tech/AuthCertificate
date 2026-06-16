7e41eb849128e9ac393709216ba512954bbb0556
Gaurav Shegekar <gauravrshegekar@gmail.com>
2026-06-16 10:35:19 +0530

    margin increase

--- src/components/certificates/certificate-details.tsx
+++ src/components/certificates/certificate-details.tsx
@@ -79,7 +79,7 @@ export function CertificateDetails({
 
   return (
     <div className="min-h-screen bg-[#f1f1f1]  ">
-      <div className="max-w-md mx-auto bg-white min-h-screen my-[30px] rounded-[10px] shadow-md">
+      <div className="max-w-md mx-auto bg-white min-h-screen my-[50px] rounded-[10px] shadow-md">
 
         {/* Header */}
         <div className="bg-green-600 text-white px-4 py-4 shadow-sm rounded-[10px] ">
